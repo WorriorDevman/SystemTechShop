84082e639321d87853d8d3cd7b9e98c741a2455d
WorriorDevman <worriordevman@sys.com>
2021-07-17 18:58:22 +0700

    StartApp

---
+++ lib/utilities/my_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'SystemTechShop';
+
+  //Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccounts = '/createAccounts';
+  static String routeBuyerServices = '/buyerServices';
+  static String routeSellerServices = '/sellerServices';
+  static String routeRiderServices = 'riderServices';
+}
